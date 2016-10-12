e6a43c38ac3c4e56f70d2eb5577a82f43d9f6676
790337 <790337@10.9.30.83>
2016-10-12 13:26:48 -0500

    ahhh

--- ACS2016/src/reviewLabs/HistoryVisualization.java
+++ ACS2016/src/reviewLabs/HistoryVisualization.java
@@ -50,9 +50,10 @@ public class HistoryVisualization {
 	}
 	
 	public boolean hasNum(String string){
+		int length = string.length();
 		Scanner scan = new Scanner(string);
-		if(scan.hasNextInt()){
-			return true;
+		for(int i=0; i<length; i++){
+			//if(string.charAt(i)=="[\\d]")
 		}
 		return false;
 	
