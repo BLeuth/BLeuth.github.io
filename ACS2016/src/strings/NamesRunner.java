5bb0be17905cd9d20a5d28f24c4d3be626cbe657
BLeuth <790337@10.9.30.83>
2016-11-02 13:26:08 -0500

    Finished Checker and Ripper, started names Generator

---
+++ ACS2016/src/strings/NamesRunner.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class NamesRunner {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Names Generator");
+		
+		NamesGenerator bam = new NamesGenerator();
+	}
+
+}
