2a763e5175f2c06305a179a8306e01638c5872c0
BLeuth <790337@10.9.30.83>
2016-11-07 15:33:20 -0600

    Finished NamesGenerator

--- ACS2016/src/strings/NamesGenerator.java
+++ ACS2016/src/strings/NamesGenerator.java
@@ -2,4 +2,52 @@ package strings;
 
 public class NamesGenerator {
 
+	String fName="";
+	String lName="";
+	String color="";
+	String finalName="";
+	
+	public NamesGenerator(String f, String l, String c){
+		fName=f;
+		lName=l;
+		color=c;
+		
+		addPrefix();
+		jumbleStuff();
+		addSuffix();
+	}
+	
+	public void addPrefix(){
+		int random=(Math.random()<=.5) ? 1:2;
+		if(random==1)
+			finalName= "Mr. ";
+		else
+			finalName= "Ms. ";
+	}
+	
+	public void jumbleStuff(){
+		int rand = (int)(Math.random()*5+1);	
+		if(rand==1)
+			finalName+= color+fName;
+		else if(rand==2)
+			finalName+= color+lName;
+		else if(rand==3)
+			finalName+= lName.substring(0, (lName.length()/2))+color;
+		else if(rand==4)
+			finalName+= color.substring(0, color.length()/2)+lName;
+		else if(rand==5)
+			finalName+= color+lName.substring(0,lName.length()/2)+" "+fName.substring(0, fName.length()/2);
+	}
+	
+	public void addSuffix(){
+		int rand = (int)(Math.random()*3+1);
+		if(rand==1)
+			finalName+=" I";
+		else if(rand==2)
+			finalName+=" XXX";
+	}
+
+	public String toString(){
+		return finalName;
+	}
 }
--- ACS2016/src/strings/NamesRunner.java
+++ ACS2016/src/strings/NamesRunner.java
@@ -1,11 +1,38 @@
 package strings;
 
+import java.util.Scanner;
+
 public class NamesRunner {
 
 	public static void main(String[] args) {
+		
+		boolean cont = true;
+		
 		System.out.println("Welcome to Names Generator");
 		
-		NamesGenerator bam = new NamesGenerator();
+		while(cont){
+			Scanner scan =new Scanner(System.in);
+			
+			System.out.print("\nEnter your first name: ");
+			String fName = scan.next();
+			System.out.print("Enter your last name: ");
+			String lName = scan.next();
+			System.out.print("Enter your favorite color: ");
+			String fColor =scan.next();
+
+			
+			NamesGenerator bam = new NamesGenerator(fName,lName,fColor);
+			System.out.println("\nName:\n"+bam);
+			
+			System.out.println("\nWould you like to go again? (y/n)");
+			String more = scan.next();
+			
+			if(more.equalsIgnoreCase("n")){
+				cont = false;
+				scan.close();
+			}
+		}
+		System.out.println("\nThanks for using NameGenerator");
 	}
 
 }
