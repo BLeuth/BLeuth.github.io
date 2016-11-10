5d96cff69ab26d8761b2549e52f8a2ace1fb6d3c
BLeuth <790337@10.9.30.83>
2016-11-10 13:25:41 -0600

    Started Pirate

---
+++ ACS2016/src/strings/Pirate.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class Pirate {
+
+	String text = "";
+	
+	public Pirate(String string){
+		text=string;
+	}
+	
+	public String pirateText(){
+		String pText="";
+		
+		return pText;
+	}
+	
+	public String toString(){
+		return pirateText();
+	}
+}
